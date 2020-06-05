cda55f40552df50b05af3f8d21c7fe9b40e112f2
Greg Panciera <gregpanciera@gmail.com>
2020-06-05 00:18:24 -0500

    project presentation commit

--- client/components/Note.jsx
+++ client/components/Note.jsx
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
 import {useSpring, animated} from 'react-spring'
+// import Draggable from 'react-draggable';
+import { useDrag } from 'react-use-gesture'
 
 const Note = (props) => {
-  const summary = props.note.summary;
-  // const type = JSON.stringify(props.note.type);
-  // const note = props.note.note;
-  // console.log("Note -> props.note", props.note)
-  // console.log("Note -> note", note)
-  // const related = props.note.related | []; 
-  // let balls = {"note": {"_text": "This is a test note bro"}};
-  const springy = useSpring({opacity: 1, from: {opacity: 0}});
-  
-  // onClick={ props.clickHandler }
 
-  return (
-    <animated.div style={springy} className="noteCard">
+  const summary = props.note.summary;
+    
+  // See: https://use-gesture.netlify.app/docs/state
+  const [{ x, y }, set] = useSpring(() => ({ x: 0, y: 0 }))
+
+  // Set the drag hook and define component movement based on gesture data
+  const bind = useDrag(({ offset: [x, y] }) => set({ x, y }))
+
+  // Bind it to a component
+  return ( 
+    <animated.div {...bind()} style={{ x, y }} className="noteCard">
       <ul className="noteDetailsList">
-        <li className="noteDetail">{summary}</li>
+        <li className="noteDetail" onClick={props.onClick}>{summary}</li>
       </ul>
     </animated.div>
-  )
+  )  
 }
+
 export default Note;
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import {useSpring, animated} from 'react-spring'
+// import Draggable from 'react-draggable';
+// // import { useDrag } from 'react-use-gesture'
+
+// const Note = (props) => {
+
+//   const summary = props.note.summary;
+//   const springy = useSpring({opacity: 1, from: {opacity: 0}});
+  
+//   return (
+//     <Draggable handle=".noteCard">
+//       <animated.div style={springy} className="noteCard">
+//         <ul className="noteDetailsList">
+//           <li className="noteDetail" onClick={props.onClick}>{summary}</li>
+//         </ul>
+//       </animated.div>
+//     </Draggable>
+//   )
+// }
+// export default Note;
